d360d2651d2bbcc80d761ff6411f6b6ff33a54c6
Yuri Almeida <yurialves2@gmail.com>
2020-12-30 02:20:25 -0300

    init course 4

---
+++ 8.kotlin-exceptions-null-safety/src/br/com/alura/bytebank/Main.kt
@@ -0,0 +1,21 @@
+package br.com.alura.bytebank
+
+fun main() {
+    println("inicio main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1() {
+    println("inicio funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("inicio funcao2")
+    for (i in 1..5) {
+        println(i)
+    }
+    println("fim funcao2")
+}
