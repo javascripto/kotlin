a0fa6e31a42d14b73a43b95cd6d9c26bed1fdcb1
Yuri Almeida <yurialves2@gmail.com>
2021-01-01 16:09:39 -0300

    finish course 4

--- 8.kotlin-exceptions-null-safety/src/br/com/alura/bytebank/Main.kt
+++ 8.kotlin-exceptions-null-safety/src/br/com/alura/bytebank/Main.kt
@@ -1,21 +1,30 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
+import br.com.alura.bytebank.teste.testaComportamentosConta
+
 fun main() {
-    println("inicio main")
-    funcao1()
+    println("início main")
+    testaComportamentosConta()
     println("fim main")
 }
 
 fun funcao1() {
-    println("inicio funcao1")
-    funcao2()
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: SaldoInsuficienteException) {
+        e.printStackTrace()
+        println("SaldoInsuficienteException foi pegada")
+    }
     println("fim funcao1")
 }
 
 fun funcao2() {
-    println("inicio funcao2")
+    println("início funcao2")
     for (i in 1..5) {
         println(i)
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
