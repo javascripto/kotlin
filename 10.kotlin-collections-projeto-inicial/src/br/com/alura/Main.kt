24dc08a30b74981d123f8251bfc9928be912a097
Yuri Almeida <yurialves2@gmail.com>
2021-01-12 22:33:56 -0300

    links

---
+++ 10.kotlin-collections-projeto-inicial/src/br/com/alura/Main.kt
@@ -0,0 +1,23 @@
+package br.com.alura
+
+//https://kotlinlang.org/docs/reference/extensions.html
+// https://kotlinlang.org/docs/reference/exceptions.html#checked-exceptions
+//https://en.wikipedia.org/wiki/Delegation_pattern
+//https://kotlinlang.org/docs/reference/delegated-properties.html
+//https://kotlinlang.org/docs/reference/scope-functions.html
+//https://medium.com/luizalabs/kotlin-let-run-with-also-ou-apply-24e8745f12fd
+//https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/union.html
+
+//https://kotlinlang.org/docs/reference/collections-overview.html
+//https://kotlinlang.org/docs/reference/iterators.html
+
+//https://kotlinlang.org/docs/reference/operator-overloading.html
+//https://kotlinlang.org/docs/reference/collection-ordering.html
+//https://kotlinlang.org/docs/reference/fun-interfaces.html
+//https://kotlinlang.org/docs/reference/functions.html#infix-notation
+
+// criar distinct, union, subtract, intersect no typescript para arrays, sets, maps
+
+fun main() {
+
+}
